cc9b78a95b711e5e99bfec582257ecb9eddc8164
Stephen Domican <stephen.p.domican@gmail.com>
2025-05-06 19:17:40 +0100

    Added HexGrid package
    Added HexGrid package

--- src/renderer/src/pages/HexTest.tsx
+++ src/renderer/src/pages/HexTest.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HexGrid, Layout, Hexagon, Text, Pattern, Path, Hex } from 'react-hexgrid';
 import { Link } from 'react-router-dom';
 
 export default function HexTest(): React.JSX.Element {
@@ -8,6 +9,11 @@ export default function HexTest(): React.JSX.Element {
       <p>
         <Link to="/">← Back to Home</Link>
       </p>
+      <HexGrid>
+        <Layout size={{ x: 10, y: 10 }} flat={true} spacing={1.1} origin={{ x: 0, y: 0 }}>
+          <Hexagon q={0} r={0} s={0} />
+        </Layout>
+      </HexGrid>
     </div>
   );
 }
