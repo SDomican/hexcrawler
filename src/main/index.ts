dbc16186df85a607b3e3789ddca5bd0f7e11a9c2
Stephen Domican <stephen.p.domican@gmail.com>
2025-05-25 09:50:35 +0100

    Added min height and width

--- src/main/index.ts
+++ src/main/index.ts
@@ -8,6 +8,8 @@ function createWindow(): void {
   const mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
+    minHeight: 670,
+    minWidth: 900,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
