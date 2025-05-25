8c4a5192a5a8d3e0a1dc78a5b805998140ce4e34
Stephen Domican <stephen.p.domican@gmail.com>
2025-05-25 08:23:37 +0100

    Button css and fixed hot reloading

--- src/renderer/src/components/ButtonContainer.tsx
+++ src/renderer/src/components/ButtonContainer.tsx
@@ -1,9 +1,11 @@
 import styles from "./buttonContainer.module.css";
+import Button from '@renderer/components/Button';
+import gearIcon from '../assets/Images/gear-hammer.svg';
 
 function ButtonContainer(): React.JSX.Element {
   return (
     <div className={styles.container}>
-
+      <Button iconSrc={gearIcon}/>
     </div>
   );
 }
--- src/renderer/src/pages/PixiTest.tsx
+++ src/renderer/src/pages/PixiTest.tsx
@@ -4,10 +4,7 @@ import React from 'react';
 import PixiScreen from '@renderer/components/PixiScreen';
 import SideBar from '@renderer/components/SideBar';
 import PixiScreenHexTest from '@renderer/components/PixiScreenHexTest';
-// @ts-ignore
-import Button from '@renderer/components/Button';
-// @ts-ignore
-import gearIcon from '../assets/Images/gear-hammer.svg';
+
 
 import ButtonContainer from '@renderer/components/ButtonContainer';
 
@@ -18,7 +15,6 @@ export default function PixiTest(): React.JSX.Element {
       <div id="PixiTest" style={{ width: '100%', height:'100%', border: '4px solid purple',  display: 'flex' }}>
           <SideBar/>
           <PixiScreenHexTest/>
-          {/* <Button iconSrc={gearIcon}/> */}
           <ButtonContainer/>
       </div>
     </>
